71beffef398927281865189c15e24d5ea40fee2a
Divarius <dima.skalskij@gmail.com>
2017-01-01 16:13:54 +0200

    fix server

--- webpack.server.js
+++ webpack.server.js
@@ -1,5 +1,6 @@
 let path = require('path');
 let fs = require('fs');
+const LOADERS = require("./config/webpack.loaders");
 
 let config = {
 	externals: [
@@ -30,53 +31,7 @@ let config = {
 	},
 
 	module: {
-		loaders: [
-			{
-				test: /\.json$/,
-				loader: 'json-loader'
-			},
-			{
-				test: /\.tsx?$/,
-				loader: 'ts-loader',
-				exclude: /node_modules/
-			},
-			{
-				test: /\.css$/,
-				exclude: [path.resolve('./node_modules/@cian/components')],
-				loaders: [
-					'isomorphic-style-loader',
-					'css-loader?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]'
-				]
-			},
-			{
-				test: /\.css$/,
-				include: [path.resolve('./node_modules/@cian/components')],
-				loaders: [
-					'isomorphic-style-loader',
-					'css-loader?modules&importLoaders=2&localIdentName=[local]'
-				]
-			},
-			{
-				test: /\.eot(\?.*)?$/,
-				loader: 'file?name=h/[hash].[ext]'
-			},
-			{
-				test: /\.(woff|woff2)(\?.*)?$/,
-				loader: 'file-loader?name=h/[hash].[ext]'
-			},
-			{
-				test: /\.ttf(\?.*)?$/,
-				loader: 'url?limit=10000&mimetype=application/octet-stream&name=h/[hash].[ext]'
-			},
-			{
-				test: /\.svg(\?.*)?$/,
-				loader: 'url?limit=10000&mimetype=image/svg+xml&name=h/[hash].[ext]'
-			},
-			{
-				test: /\.(jpe?g|png|gif)$/i,
-				loader: 'url?limit=1000&name=h/[hash].[ext]'
-			}
-		]
+		loaders: LOADERS.COMMON.concat(LOADERS.DEV)
 	},
 
 	plugins: [],
